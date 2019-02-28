3077d1ef8d38023ec4d4772d96d3edb8204565bf
Alberto <alberto.cabrera@serviciosreunidos.es>
2019-02-28 15:16:53 +0100

    Se muestran las preguntas recibidas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const electron = require('electron');
 const {app, BrowserWindow, ipcMain} = require('electron');
+const axios = require('axios');
 
 // Adds debug features like hotkeys for triggering dev tools and reload
 require('electron-debug')();
@@ -26,6 +27,12 @@ function createMainWindow() {
 	return win;
 }
 
+async function loadData() { 
+	const response = await axios.post('https://opentdb.com/api.php?amount=1', '');
+	console.log(response.data.results);
+	return response.data.results[0];
+}
+
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
@@ -40,11 +47,10 @@ app.on('activate', () => {
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
+	loadData();
 });
 
-ipcMain.on('asyncChannelToMain', (event, arg) => {
-	console.log(arg + ' from renderer')
-	if (arg === 'hello') {
-	  event.sender.send('asyncChannelToRenderer', 'world')
-	}
-  })
+ipcMain.on('load-data', async (event, arg) => {
+	const data = await loadData();
+	event.sender.send('load-data-result', data)
+});

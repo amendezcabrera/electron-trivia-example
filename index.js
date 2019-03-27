7d259e30d12ae505d4e5cdc28d1aab57188ff5e8
Alberto <alberto.cabrera@serviciosreunidos.es>
2019-03-27 13:42:19 +0100

    Added answer check

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ require('electron-debug')();
 // Prevent window being garbage collected
 let mainWindow;
 
+exports.currentResultData = [];
+//var currentResultData = [];
+
 function onClosed() {
 	// Dereference the window
 	// For multiple windows store them in an array
@@ -20,10 +23,8 @@ function createMainWindow() {
 		width: 500,
 		height: 600
 	});
-
 	win.loadURL(`file://${__dirname}/index.html`);
 	win.on('closed', onClosed);
-
 	return win;
 }
 
@@ -57,10 +58,24 @@ app.on('window-all-closed', () => {
 	}
 });
 
-app.on('ready', () => {
+app.on('ready', async () => {
 	mainWindow = createMainWindow();
 });
 
 ipcMain.on('load-data', async (event, arg) => {
-	event.sender.send('load-data-result', (await getResultData()));
+	await refreshQuestion(event);
+});
+
+async function refreshQuestion(event){
+	exports.currentResultData = await getResultData();
+	event.sender.send('load-data-result', exports.currentResultData);
+}
+
+ipcMain.on('answer', async (event, arg) => {
+	if(arg === exports.currentResultData.correct_answer){
+		console.log('Yeahhhhh!!');
+	}else{
+		console.log(`Wroooong!! (${exports.currentResultData.correct_answer})`);
+	}
+	await refreshQuestion(event);
 });
